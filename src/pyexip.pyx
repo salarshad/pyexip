7671b3847b0ea7a0032c6f3e068f9168f0bfdec4
salarshad <salmanarshad@outlook.com>
2012-12-21 03:13:47 +0500

    Corrected malloc() and free() usages. Added __repr__ for BinaryBuffer

--- src/pyexip.pyx
+++ src/pyexip.pyx
@@ -1,8 +1,11 @@
 cimport pyexipd
 
-from libc.stdlib cimport malloc, free
+from libc.stdlib cimport malloc, calloc, free
 cdef extern from "stdlib.h":
      void *memcpy(void *dst, void *src, long n)
+     
+cdef extern from "stdlib.h":
+     void *memset( void * ptr, int value, size_t num)  
 
 cdef extern from "Python.h":
     object PyString_FromStringAndSize(char *, Py_ssize_t)
@@ -28,22 +31,33 @@ cdef class EXIStream:
     # def __cinit__(self):
     #     self.header = __EXI_Header(self.obj)
 
-cdef class  String:
+cdef class String:
     cdef pyexipd.String obj
     cdef char * str_ptr
+    cdef int malloced
     def __cinit__(self, the_string):
+        self.malloced = 0
         self.obj = pyexipd.String()
         self.str_ptr = NULL
 
-        self.obj.str = <pyexipd.CharType*>malloc(len(the_string))
-        if self.obj.str is not NULL:
-            self.str_ptr = PyString_AsString(the_string)
-            memcpy(self.obj.str, self.str_ptr, len(the_string))
-            self.obj.length = len(the_string)
+        if len(the_string) > 0:
+            self.obj.str = <char*>calloc(len(the_string) + 1, sizeof(char))
+            if self.obj.str is not NULL:
+                self.malloced = 1
+                self.str_ptr = PyString_AsString(the_string)
+                memset(self.obj.str, 0, len(the_string) + 1)
+                memcpy(self.obj.str, self.str_ptr, len(the_string))
+                self.obj.length = len(the_string)
+        else:
+            self.obj.str = NULL
+            self.obj.length = 0
 
     def __dealloc__(self):
-        if self.obj.str is not NULL:
+        if self.obj.str is not NULL and self.malloced == 1:
+            #print " ---- __dealloc__ (%d) %s" % (self.obj.length, self.obj.str)
             free(self.obj.str)
+            self.obj.str = NULL
+            
 
 cdef class QName:
     cdef pyexipd.QName obj
@@ -66,7 +80,9 @@ cdef class QName:
         def __del__(self):
             self.obj.localName = NULL   
 
-
+#define SET_STRICT(p)                 ((p) = (p) | STRICT)
+# def SET_STRICT(int):
+#     pass
 
 def asciiToString(char * the_string, String ch, EXIStream strm):
     pyexipd.asciiToString(the_string, &ch.obj, &strm.obj.memList, pyexipd.FALSE)
@@ -82,12 +98,13 @@ cdef class serialize:
     def setHasCookie(self, EXIStream strm):
         strm.obj.header.has_options = 1
         strm.obj.header.has_cookie = 1
+        #pyexipd.SET_STRICT(strm.obj.header.opts.enumOpt)
 
     def initStream(self, EXIStream strm, BinaryBuffer buffer):
         pyexipd.initStream(&strm.obj, buffer.obj, NULL, pyexipd.SCHEMA_ID_ABSENT, NULL)
 
     def exiHeader(self, EXIStream strm):
-        pyexipd.exiHeader(&strm.obj)
+        pyexipd.encodeHeader(&strm.obj)
 
     def startDocument(self, EXIStream strm):
         pyexipd.startDocument(&strm.obj)
@@ -118,17 +135,30 @@ cdef class BinaryBuffer:
         self.initialize(size_in_bytes)
 
     def initialize(self, size_t size_in_bytes):
-        self.obj.buf = <char*>malloc(size_in_bytes * sizeof(char))
+        self.obj.buf = <char*>calloc(size_in_bytes * sizeof(char), sizeof(char))
         self.obj.bufLen = size_in_bytes
         self.obj.bufContent = 0
         self.obj.ioStrm.stream = NULL
+
+    def content(self):
+        return self.obj.bufContent
+
+        
         
     def __len__(self):
         return self.obj.bufLen
-  
+        
+    def __repr__(self):
+        cdef Py_ssize_t length = self.obj.bufLen
+        cdef bytes py_string = self.obj.buf[:length]
+        return py_string
+        
     def __dealloc__(self):
         if self.obj.buf is not NULL:
             free(self.obj.buf)
+            self.obj.buf = NULL
+            
+    
 
 #    cdef struct ccEXIheader:
         # unsigned char has_cookie
@@ -140,7 +170,7 @@ cdef class BinaryBuffer:
 cdef class EXIheader:
     cdef pyexipd.cEXIheader obj
     def __cinit__(self, unsigned char has_cookie, unsigned char has_options):
-        memcpy(&self.obj, NULL, sizeof(pyexipd.cEXIheader))
+        memset(&self.obj, 0, sizeof(pyexipd.cEXIheader))
 
         self.obj.has_cookie = has_cookie
         self.obj.has_options = has_options
