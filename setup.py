9ebbf419cc96108eeb2d7b8ff78aabbe83bc5035
salarshad <salmanarshad@outlook.com>
2012-12-24 17:00:47 +0500

    Simplified setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
-from distutils.command.install import install as DistutilsInstall
-from distutils.command.build_ext import build_ext as DistutilsBuildExt
+#from distutils.command.install import install as DistutilsInstall
 import subprocess
 import os.path
 import os
 import shutil
 
-class BuildExip(DistutilsBuildExt):
+class BuildExip(build_ext):
     def run(self):
         os.chdir(os.path.join("exip-0.4.1", "build", "gcc"))
         subprocess.call("make lib",  shell=True)
         os.chdir("../../../")
-        DistutilsBuildExt.run(self)
+        build_ext.run(self)
 
 src_dir = "src"
 sources = ["pyexip.pyx"]
@@ -29,8 +28,8 @@ setup(
     description="An EXI (Efficient XML Interchange) solution for Python based on EXIP.",
     author="Salman Arshad",
     cmdclass = {'build_ext': BuildExip},
-    ext_modules = cythonize([Extension("pyexip", 
-                                        sources_with_path, 
-                                        extra_objects=[os.path.join("exip-0.4.1", "bin", "lib", "libexip.a")]
-                                        )])
+    ext_modules = [Extension("pyexip", 
+                            sources_with_path, 
+                            extra_objects=[os.path.join("exip-0.4.1", "bin", "lib", "libexip.a")]
+                            )]
 )
